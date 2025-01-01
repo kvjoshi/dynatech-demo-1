407b01eabc373cf40ac79da0ce1f1d95f0f83901
Aarsh Shukla <invinciblearjuna1@gmail.com>
2025-01-01 19:47:27 +0530

    chore: navbar style

--- src/app/dashboard/components/navbar/Navbar.tsx
+++ src/app/dashboard/components/navbar/Navbar.tsx
@@ -2,17 +2,17 @@ import { CircleUserRound, HomeIcon, LogOut } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between bg-[#002f6c] h-12 px-4 rounder-br-full rounded-bl-full">
-      <div className="flex items-center bg-[#E64000] h-full px-4 rounded-bl-full rounded-br-full text-white mr-2">
+    <div className="flex items-center justify-between bg-[#002f6c] h-12 px-4 rounder-br-3xl rounded-bl-3xl">
+      <div className="flex items-center bg-[#E64000] h-full px-4 rounded-bl-3xl rounded-br-3xl text-white ">
         <HomeIcon />
         <span className="text-white font-semibold text-sm">Dashboard</span>
       </div>
-      <div className="flex items-center space-x-4">
-        <div className="bg-white flex items-center justify-center rounded-full w-8 h-8">
-          <CircleUserRound />
+      <div className="flex items-center space-x-4 bg-white p-2 rounded-3xl">
+        <div className="bg-[#002f6c] flex items-center justify-center rounded-full w-8 h-8">
+          <CircleUserRound fill="white" />
         </div>
-        <div className="bg-white flex items-center justify-center rounded-full w-8 h-8">
-          <LogOut />
+        <div className="bg-[#002f6c] flex items-center justify-center rounded-full w-8 h-8">
+          <LogOut color="white" />
         </div>
       </div>
     </div>
