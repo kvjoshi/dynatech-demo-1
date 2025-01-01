ee4007b4fa15a3b92f0f43cbcaa7376b0ea51fa9
Bhargav-92 <tkr.bhargav92@gmail.com>
2025-01-01 19:08:59 +0530

    feat: sidebar

--- src/app/dashboard/components/sidebar/Sidebar.tsx
+++ src/app/dashboard/components/sidebar/Sidebar.tsx
@@ -1,31 +1,136 @@
-import { User, Users } from "lucide-react";
+import {
+  Calendar,
+  CalendarCheck,
+  ClipboardList,
+  History,
+  HomeIcon,
+  IdCard,
+  LogOut,
+  MessageSquareMore,
+  MessageSquareText,
+  Shield,
+  TableOfContents,
+  User,
+  Users,
+} from "lucide-react";
 import saicia from "../../../../assets/saica-logo-white-2.png";
 
-export default function Sidebar() {
+const Sidebar = () => {
   return (
     <div>
       <aside
         id="default-sidebar"
-        className="fixed top-0 left-0  w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#004990]"
+        className="fixed top-0 left-0 w-80 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#004990]"
         aria-label="Sidebar">
-        <div>
-          <div>
-            <img src={saicia} alt="" className="w-[90%] m-4" />
+        <div className="flex flex-col h-full">
+          {/* Logo */}
+          <div className="p-4">
+            <img src={saicia} alt="SAICA Logo" className="w-[90%]" />
           </div>
 
-          <div>
-            <div className="bg-white rounded-full w-16 h-16 flex items-center justify-center ">
-              <User
-                fill="#004990"
-                color="#004990"
-                strokeWidth={0.5}
-                size={40}
-              />
+          {/* User Profile */}
+          <div className="px-4 py-6 text-white ">
+            <div className="flex flex-col items-center mb-4 justify-center">
+              <div className="bg-white rounded-full w-16 h-16 flex items-center justify-center mb-3">
+                <User
+                  fill="#004990"
+                  color="#004990"
+                  strokeWidth={0.5}
+                  size={40}
+                />
+              </div>
+              <h1 className="text-lg font-semibold mb-2">Bella Paul</h1>
+              <div className="flex items-center gap-2 mb-2">
+                <Users fill="white" color="white" strokeWidth={1.5} size={20} />
+                <div className="flex flex-col">
+                  <span className="text-white text-sm">Member (CA)</span>
+                  <span className="text-gray-300 text-xs">Membership</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-2 mb-2">
+                <IdCard color="white" />
+                <div>
+                  <div className="text-gray-300 text-sm">SAICA ID</div>
+                  <div className="text-white text-xs">78976789</div>
+                </div>
+              </div>
+              <div className="flex items-center gap-2 mb-2">
+                <Calendar />
+                <div>
+                  <div className="text-gray-300 text-sm">Member since</div>
+                  <div className="text-white text-xs">12/11/2023</div>
+                </div>
+              </div>
+              <div className="text-sm mt-2">
+                <div className="flex gap-2">
+                  <Users
+                    fill="white"
+                    color="white"
+                    strokeWidth={1.5}
+                    size={20}
+                  />
+                  <div className="text-white">Responsible Partner</div>
+                </div>
+                <div className="flex gap-2">
+                  <Users
+                    fill="white"
+                    color="white"
+                    strokeWidth={1.5}
+                    size={20}
+                  />
+                  <div className="text-white">Registered Tax Practitioner</div>
+                </div>
+              </div>
             </div>
-            <div>
-              <h1>Bella Paul</h1>
-              <div>
-                <Users fill="white" color="white" />
+
+            {/* Navigation Menu */}
+            <div className="mt-6 grid grid-cols-2 bg-[#F2FBFE]  gap-5 p-6">
+              <div className="flex flex-col items-center justify-center text-center bg-[#EA1946] rounded-tl-full rounded-bl-full rounded-tr-full p-6">
+                <HomeIcon color="white" />
+                <span className="white">Dashboard</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <TableOfContents color="#004990" />
+                <span className="text-[#004990]">Application</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <div className="bg-[#004990] rounded-full w-6 h-6 flex items-center justify-center mb-3">
+                  <User
+                    fill="white"
+                    color="white"
+                    strokeWidth={0.5}
+                    size={40}
+                  />
+                </div>
+                <span className="text-[#004990]">Profile</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <MessageSquareMore color="white" fill="#004990" />
+                <span className="text-[#004990]">Queries</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <MessageSquareText color="white" fill="#004990" />
+                <span className="text-[#004990]">Membership Compliance</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <Shield color="white" fill="#004990" />
+                <span className="text-[#004990]">Complaint</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <CalendarCheck color="white" fill="#004990" />
+                <span className="text-[#004990]">Upcoming Events</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <ClipboardList color="white" fill="#004990" />
+                <span className="text-[#004990]">Exam</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <History color="white" fill="#004990" />
+                <span className="text-[#004990]">Exam</span>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center hover:bg-gray-200 hover:rounded-tl-full hover:rounded-bl-full hover:rounded-tr-full p-6 ">
+                <LogOut color="#004990" />
+                <span className="text-[#004990]">Logout</span>
               </div>
             </div>
           </div>
@@ -33,4 +138,6 @@ export default function Sidebar() {
       </aside>
     </div>
   );
-}
+};
+
+export default Sidebar;
