535876363ed6773699e9eeb68d8239b0b95ad43b
Vimal Sonara <vvsonara95@gmail.com>
2025-01-18 14:54:16 +0530

    feat(mobile): community-detail page

---
+++ mobile-filter.js
@@ -0,0 +1,11 @@
+function toggleFilter() {
+  const filter = document.querySelector(".filter-content");
+  filter.className = "";
+  filter.classList.add("mobile-filter");
+}
+
+function closeFilter() {
+  const filter = document.querySelector(".mobile-filter");
+  filter.className = "";
+  filter.classList.add("filter-container");
+}
